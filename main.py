215cdd178db84caf72ea55ae4dd1352ba4c3d50a
Cartsist <astakhovda1209@mail.ru>
2024-02-03 16:30:42 +0300

    zadanie 1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ for i in a:
         print('У персонажа '+i[1]+'в игре '+i[0]+'нашлась ошибка с кодом ' +i[2]+'.' +' Дата фиксации: ',i[-1])
         i[2]='Done'
         i[-1] = '0000-00-00'
-
 file = open('game_new.csv', "w")
 for i in a:
-    file.write(('\t'.join(i))+'\n')
+    if i[-1]=='0000-00-00':
+        file.write(('\t'.join(i))+'\n')
